1e27176b1590fc0a04c35d8d90fbd6c617a62db5
Garbakbl <121131132+Garbakbl@users.noreply.github.com>
2023-04-13 00:52:44 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,8 @@
-import pandas as pd
+from scipy.stats import ks_2samp
 import numpy as np
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 356550601
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    _, p_value = ks_2samp(x, y)
+    return p_value >= 0.07
